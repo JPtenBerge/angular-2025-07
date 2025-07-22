610937987218c4e3f9fae9a5d28b8297ed062902
JP <jp@jp.nl>
2025-07-22 14:25:33 +0200

    Backendcommunicatie laten zien met HttpClient

--- demo/src/app/app.config.ts
+++ demo/src/app/app.config.ts
@@ -2,11 +2,18 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { NavigateService } from './services/navigate.service';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+	providers: [
+		// globale instellingen / dependency injection
+		provideBrowserGlobalErrorListeners(),
+		provideZonelessChangeDetection(),
+		provideRouter(routes),
+		provideHttpClient(),
+
+		// { provide: NavigateService, useClass: NavigateService }
+		// NavigateService // tree-shaking optimization
+	],
 };
--- demo/src/app/app.ts
+++ demo/src/app/app.ts
@@ -1,8 +1,9 @@
 import { JsonPipe } from '@angular/common';
-import { Component, viewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { LifeComponent } from './components/life.component';
 import { Autocompleter } from './components/autocompleter/autocompleter';
+import { HttpClient } from '@angular/common/http';
 
 interface Animal {
 	id: number;
@@ -19,38 +20,23 @@ interface Animal {
 })
 export class App {
 	showLife = false;
-	animals: Animal[] = [
-		{
-			id: 4,
-			species: 'Cat',
-			maxAge: 18,
-			photoUrl:
-				'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpaperaccess.com%2Ffull%2F2111331.jpg&f=1&ipt=5f9d5319f73bfdd87ddebf52bf88bb6533c031a74b51ba43303bd5a149cc3892',
-		},
-		{
-			id: 8,
-			species: 'Dog',
-			maxAge: 20,
-			photoUrl:
-				'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2018%2F10%2F06%2F364377-puppies-puppy-baby-dog-dogs-41.jpg&f=1&nofb=1&ipt=672823a750bbd3e16f68e2bf4d17d42b82890441794ecbdda3c9465c7ac45ef4',
-		},
-		{
-			id: 15,
-			species: 'Parrot',
-			maxAge: 120,
-			photoUrl:
-				'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fanimalia-life.club%2Fdata_images%2Fparrot%2Fparrot4.jpg&f=1&nofb=1&ipt=08373a91ce5d5a56ada1f3b0df8af9ab0a19eb8416e4e5eba50c17898a21523c',
-		},
-	];
+	animals?: Animal[];
 	newAnimal = {} as Animal;
 
 	autocompleter = viewChild(Autocompleter<object>);
 
-	addAnimal() {
-		this.animals.push({
-			...this.newAnimal,
-			id: Math.max(...this.animals.map(x => x.id)) + 1,
+	http = inject(HttpClient);
+	cdr = inject(ChangeDetectorRef);
+
+	ngOnInit() {
+		this.http.get<Animal[]>('http://localhost:3000/animals').subscribe(animals => {
+			this.animals = animals;
+			this.cdr.markForCheck();
 		});
+	}
+
+	addAnimal() {
+		this.http.post('http://localhost:3000/animals', this.newAnimal).subscribe();
 	}
 
 	increaseMaxAge(animal: Animal) {
