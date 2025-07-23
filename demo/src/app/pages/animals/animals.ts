9f31bf1d5ebe662640049f548c8d5c95283cd326
JP <jp@jp.nl>
2025-07-23 10:01:57 +0200

    Reactive forms laten zien

--- demo/src/app/pages/animals/animals.ts
+++ demo/src/app/pages/animals/animals.ts
@@ -1,15 +1,19 @@
 import { JsonPipe } from '@angular/common';
 import { ChangeDetectorRef, Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 import { Loading } from '../../components/loading/loading';
 import { Animal } from '../../models/animal';
 
+function mijnCustomValidator(c: AbstractControl) {
+	return null;
+}
+
 @Component({
 	selector: 'app-animals',
-	imports: [RouterOutlet, FormsModule, JsonPipe, Loading],
+	imports: [RouterOutlet, FormsModule, ReactiveFormsModule, JsonPipe, Loading],
 	templateUrl: './animals.html',
 })
 export class Animals {
@@ -19,6 +23,12 @@ export class Animals {
 
 	http = inject(HttpClient);
 	cdr = inject(ChangeDetectorRef);
+
+	addAnimalForm = new FormGroup({
+		species: new FormControl<string>('', [Validators.required, Validators.pattern('[a-zA-Z -]{3,}'), mijnCustomValidator]),
+		maxAge: new FormControl<number>(0, Validators.required),
+		photoUrl: new FormControl<string>('', Validators.required),
+	});
 
 	ngOnInit() {
 		this.http.get<Animal[]>('http://localhost:3000/animals').subscribe(animals => {
@@ -30,6 +40,9 @@ export class Animals {
 
 	addAnimal() {
 		this.http.post('http://localhost:3000/animals', this.newAnimal).subscribe();
+	}
+	addAnimalReactive() {
+		this.http.post('http://localhost:3000/animals', this.addAnimalForm.value).subscribe();
 	}
 
 	increaseMaxAge(animal: Animal) {
